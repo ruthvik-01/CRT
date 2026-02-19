77d3732c2eefb496a8cdac3310e348e8b224f489
Ruthvik <ruthvikp186@gmail.com>
2026-02-19 23:03:57 +0530

    initial1

---
+++ r_a_t.java
@@ -0,0 +1,21 @@
+//right angled triangle
+
+import java.util.Scanner;
+
+public class r_a_t {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++)
+
+        {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
