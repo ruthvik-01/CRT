bae3d824a96e0a8c12b5304991203cac9baed98f
Ruthvik <ruthvikp186@gmail.com>
2026-02-26 14:52:18 +0530

    3

---
+++ Day 6/tree.java
@@ -0,0 +1,12 @@
+import java.util.TreeSet;
+
+public class rree {
+    public static void main(String[] args) {
+        TreeSet<String> s = new TreeSet<>();
+        s.add("ruthvik");
+        s.add("kiran");
+        s.add("sachin");
+        s.add("rahul");
+        System.out.println(s);
+    }
+}
